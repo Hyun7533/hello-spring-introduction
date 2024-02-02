5eedff1641fca77205b93d097726cc1400fdb88a
TaeHyun Kim <xogus7533@naver.com>
2024-02-03 08:15:14 +0900

    2024-02-03 commit

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,8 +1,14 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 /**
  * spring이 실행될때 스프링 컨테이너에 얘가 등록된다. 객체를 생성하여 들고있다.
@@ -11,10 +17,35 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class MemberController {
 
+    // 필드 주입 -
+    // 생성자 주입 - 해당 객체가 생성될때 주입이 되기때문에 권장
+    // setter 주입 - 세터 메서드가 호출이 되기때문에 권장 X
+
     private final MemberService memberService;
 
     @Autowired // 의존관계 주입 (DI)
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
     }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+        memberService.join(member);
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
+    }
+
 }
