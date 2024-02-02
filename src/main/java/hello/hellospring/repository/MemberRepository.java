199c88a8a106d65f9a717fa49fad19ec2807b685
TaeHyun Kim <xogus7533@naver.com>
2024-02-03 08:56:30 +0900

    2024-02-03 commit

--- src/main/java/hello/hellospring/repository/MemberRepository.java
+++ src/main/java/hello/hellospring/repository/MemberRepository.java
@@ -2,11 +2,12 @@ package hello.hellospring.repository;
 
 import hello.hellospring.domain.Member;
 
+import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
 
 public interface MemberRepository {
-    Member save(Member member);
+    Member save(Member member) throws SQLException;
     Optional<Member> findById(Long id); // null을 반환할때 옵셔널로 null을 반환한다
     Optional<Member> findByName(String name);
     List<Member> findAll();
